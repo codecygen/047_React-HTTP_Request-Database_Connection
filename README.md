c43f8b0e3432632db5969e4016795fd31abecc23
gingercook <vahitaras@gmail.com>
2021-12-19 01:01:20 -0500

    error handling is covered with

---
+++ README.md
@@ -0,0 +1,11 @@
+In this project, we will fetch the movies list from a Rest API.
+
+Searchable Keywords:
+- React-HTTP_Request-Fetch_API
+
+Fetch API:  
+This is a built in Javascript interface for accessing and manipulating of the HTTP pipeline.
+
+- React-HTTP_Request-Loadig_State
+
+Because fetching the data from an API will take some time, we need to introduce a loading state to let the person know if we could have fetched a data and/or if our app is still busy getting the data from the external server (API).
--- src/App.js
+++ src/App.js
@@ -1,31 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies, setMovies] = useState([]);
+  // React-HTTP_Request-Loadig_State
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // React-HTTP_Request-Fetch_API
+  const fetchMoviesHandler = async () => {
+    // React-HTTP_Request-Loadig_State
+    // loading starts here
+    setIsloading(true);
+    // We want to make sure we clear any previous errors here.
+    setError(null);
+
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/film');
+      const data = await response.json();
+
+      // data.results coming from the API object key, results is the key
+      // Check out the link to see it.
+      // Here we will transform the keys into the keys that we use in our
+      // props in "MoviesList.js"
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+
+      setMovies(transformedMovies);
+      // React-HTTP_Request-Loadig_State
+      // loading ends here
+      setIsloading(false);
+    } catch (error) {
+
+    }
+  }
+
+
+  // // Alternative asyncronous function handling
+  // // React-HTTP_Request-Fetch_API
+  // const fetchMoviesHandler = () => {
+  //   // loading starts here
+  //   setIsloading(true);
+  //   // We want to make sure we clear any previous errors here.
+  //   setError(null);
+  //   // Here the default behaviour is get request
+  //   // Default behavior of fetch API is GET request
+  //   // 'then()' section is for asyncronous function.
+  //   // Because getting the result of GET request
+  //   // might take some time.
+  //   // 'catch()' section is to catch any potential errors.
+  //   // There are 2 'then' chains here, first one transforms the
+  //   // json format to an object, second one 
+  //   fetch('https://swapi.py4e.com/api/films').then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     // data.results coming from the API object key, results is the key
+  //     // Check out the link to see it.
+  //     // Here we will transform the keys into the keys that we use in our
+  //     // props in "MoviesList.js"
+  //     const transformedMovies = data.results.map(movieData => {
+  //       return {
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date
+  //       };
+  //     });
+
+  //     setMovies(transformedMovies);
+  //     // loading ends here
+  //     setIsloading(false);
+  //   });
+  // }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {/* React-HTTP_Request-Loadig_State */}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {/* React-HTTP_Request-Loadig_State */}
+        {!isLoading && movies.length === 0 && <p>No results found!</p>}
+        {/* React-HTTP_Request-Loadig_State */}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
